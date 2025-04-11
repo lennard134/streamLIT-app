bfbef6055088f949c113420d12e2e956895155db
lennard134 <78602837+lennard134@users.noreply.github.com>
2025-04-11 12:30:48 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -95,11 +95,13 @@ def get_model_response(user_message, HF_client, model_name, max_retries=10, wait
 # UI
 
 with st.sidebar:
-    user_input = st.text_input("Please insert your unique identifier", "")
-    user_input = user_input.replace('tr', '')
-    user_input = int(user_input)
-    if not user_input:
+    user_input = st.text_input("Please insert your unique identifier", "tr...")
+
+    if user_input == "tr...":
         st.warning('Put your unique ID in the sidebar to continue', icon="⚠️")
+    else:
+        user_input = user_input.replace('tr', '')
+        user_input = int(user_input)
     if user_input < 100:
         model_name = "meta-llama/Llama-3.2-1B-Instruct"
     elif 100 <= user_input < 500:
