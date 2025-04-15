e91beb355d0cdc06673dc614f3f39aeda25dc0e0
lennard134 <78602837+lennard134@users.noreply.github.com>
2025-04-15 15:35:23 +0200

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -77,14 +77,14 @@ def get_embedding_with_retry(user_message, HF_client, max_retries=10, wait_time=
                 retries += 1
                 wait_time = wait_time * 2  # Exponentially increase wait time
                 print(f"Retrying... {retries}/{max_retries}")
-                time.sleep(wait_time)
+                # time.sleep(wait_time)
         
         except requests.exceptions.RequestException as e:
             print(f"Request failed due to error: {e}")
             retries += 1
             wait_time = wait_time * 2  # Exponentially increase wait time
             print(f"Retrying... {retries}/{max_retries}")
-            time.sleep(wait_time)
+            # time.sleep(wait_time)
     return None
     
 def get_model_response(user_message, HF_client, model_name, max_retries=10, wait_time=1):
@@ -107,14 +107,14 @@ def get_model_response(user_message, HF_client, model_name, max_retries=10, wait
                 retries += 1
                 wait_time = wait_time * 2  # Exponentially increase wait time
                 print(f"Retrying... {retries}/{max_retries}")
-                time.sleep(wait_time)
+                # time.sleep(wait_time)
         
         except requests.exceptions.RequestException as e:
             print(f"Request failed due to error: {e}")
             retries += 1
             wait_time = wait_time * 2  # Exponentially increase wait time
             print(f"Retrying... {retries}/{max_retries}")
-            time.sleep(wait_time)
+            # time.sleep(wait_time)
     
     return None
 
